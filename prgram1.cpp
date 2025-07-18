812dbda2c2a9958455a4cacf7559c9a207dc76cd
TarushiV26 <tarushiv2611@gmail.com>
2025-07-18 11:15:12 +0530

    prgram1.cpp

---
+++ prgram1.cpp
@@ -0,0 +1,15 @@
+// PROGRAM TO CHECK EVEN OR ODD
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter num 1: "<<endl;
+    cin>>a;
+    cout<<"enter num 2: "<<endl;
+    cin>>b;
+if(a>b)
+cout<<"larger number is: "<<a<<endl;
+else
+cout<<"larger number is: "<<b<<endl;
+}
